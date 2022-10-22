cf8f3a3fe35e6f7bf86c682cfdaf4276015656c5
valerycode <lera200@yandex.ru>
2022-10-22 20:29:50 +0600

    add movies app

---
+++ movies_admin/movies/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from .models import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+    raw_id_fields = ('person',)
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
+    list_display = ('title', 'type', 'creation_date', 'rating')
+    list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+    search_fields = ('full_name',)
---
+++ movies_admin/movies/models.py
@@ -0,0 +1,124 @@
+import uuid
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
+    name = models.CharField(_('name'), max_length=255, unique=True)
+    description = models.TextField(_('description'), blank=True)
+
+    class Meta:
+        db_table = "content\".\"genre"
+        ordering = ("name",)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Person(UUIDMixin, TimeStampedMixin):
+    full_name = models.CharField(
+        _('full name'),
+        max_length=255,
+        unique=True
+    )
+
+    class Meta:
+        db_table = "content\".\"person"
+        ordering = ("full_name",)
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+    def __str__(self):
+        return self.full_name
+
+
+class Filmwork(UUIDMixin, TimeStampedMixin):
+
+    class FilmworkTypes(models.TextChoices):
+        MOVIE = 'movie', _('movie'),
+        TV_SHOW = 'tv_show', _('tv_show')
+
+    title = models.CharField(
+        _('title'),
+        max_length=255,
+        unique=True
+    )
+    description = models.TextField(_('description'), blank=True)
+    creation_date = models.DateField(_('creation date'))
+    rating = models.FloatField(
+        _('rating'),
+        blank=True,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
+    type = models.CharField(
+        _('type'),
+        max_length=7,
+        choices=FilmworkTypes.choices,
+        help_text="Выберите тип произведения"
+    )
+    genres = models.ManyToManyField(Genre, through='GenreFilmwork')
+    roles = models.ManyToManyField(Person, through='PersonFilmwork')
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        ordering = ("-creation_date",)
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
+
+    def __str__(self):
+        return self.title
+
+
+class GenreFilmwork(UUIDMixin):
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"genre_film_work"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['film_work', 'genre'],
+                name='unique_film_work_genre'
+            )]
+        verbose_name = 'Жанр фильма'
+        verbose_name_plural = 'Жанры фильма'
+
+
+class PersonFilmwork(UUIDMixin):
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    role = models.TextField(_('role'), null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"person_film_work"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['film_work', 'person'],
+                name='unique_film_work_person'
+            )]
+        verbose_name = 'Участник фильма'
+        verbose_name_plural = 'Участники фильма'
